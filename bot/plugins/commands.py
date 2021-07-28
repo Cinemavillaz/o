0d3af691293b2ca7c418d5f2108dc24af9644038
Cinemavillaz <87010768+Cinemavillaz@users.noreply.github.com>
2021-07-28 15:01:24 +0530

    Update commands.py

--- bot/plugins/commands.py
+++ bot/plugins/commands.py
@@ -48,7 +48,7 @@ async def start(bot, update):
 
     buttons = [[
         InlineKeyboardButton('Developers', url='https://t.me/Cv_group1'),
-        InlineKeyboardButton('Source Code 🧾', url ='https://github.com/CrazyBotsz/Adv-Auto-Filter-Bot-V2')
+        InlineKeyboardButton('Source Code 🧾', url ='https://t.me/cv_group1')
     ],[
         InlineKeyboardButton('Support 🛠', url='https://t.me/Cv_group1')
     ],[
